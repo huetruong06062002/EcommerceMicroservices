b3750bf8cbfbf1ad33085d7c2c42eb21dc6f91e8
phanhuetruong <truongph@hisoft.com>
2026-05-11 14:25:13 +0700

    Init Entity, Dtos, Repository Pattern For Product Microservices & CRUD Product.API

--- aspnetcore-microservice/Shared/obj/Debug/net6.0/Shared.AssemblyInfo.cs
+++ aspnetcore-microservice/Shared/obj/Debug/net6.0/Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bdc7e4c852ec1f11bfd21e4ab7f81de250b25c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- aspnetcore-microservice/src/BuildingBlocks/Infrastructure/obj/Debug/net6.0/Infrastructure.AssemblyInfo.cs
+++ aspnetcore-microservice/src/BuildingBlocks/Infrastructure/obj/Debug/net6.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a5486321950d043219f32e445da21ae5ccc288a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bdc7e4c852ec1f11bfd21e4ab7f81de250b25c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
