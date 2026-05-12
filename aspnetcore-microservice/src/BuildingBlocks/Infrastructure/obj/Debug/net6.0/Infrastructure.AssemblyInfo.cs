0b22e2a5bef549347817727b47737c4d6faa154a
phanhuetruong <truongph@hisoft.com>
2026-05-12 20:09:39 +0700

    Repository Pattern For Customer. API Microservices

--- aspnetcore-microservice/src/BuildingBlocks/Infrastructure/obj/Debug/net6.0/Infrastructure.AssemblyInfo.cs
+++ aspnetcore-microservice/src/BuildingBlocks/Infrastructure/obj/Debug/net6.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94ec090ba0851f273758803a2a04960722916890")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8621a404f9db153bd5f714c6a00e966b33fee71")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- aspnetcore-microservice/src/Services/Customer.API/Program.cs
+++ aspnetcore-microservice/src/Services/Customer.API/Program.cs
@@ -6,7 +6,13 @@
 
 
 using Common.Logging;
+using Contracts.Common.Interfaces;
 using Customer.API.Persistence;
+using Customer.API.Repositories;
+using Customer.API.Repositories.Interfaces;
+using Customer.API.Services;
+using Customer.API.Services.Interfaces;
+using Infrastructure.Common;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
 
@@ -32,7 +38,25 @@ try
     builder.Services.AddDbContext<CustomerContext>(options =>
         options.UseNpgsql(connectionString));
 
+    builder.Services.AddScoped<ICustomerRepository, CustomerRepository>()
+        .AddScoped(typeof(IRepositoryBaseAsync<,,>), typeof(RepositoryBaseAsync<,,>))
+        .AddScoped(typeof(IUnitOfWork<>), typeof(UnitOfWork<>))
+        .AddScoped<ICustomerService, CustomerService>();
+
     var app = builder.Build();
+    app.MapGet("/",() => "Hello");
+
+    app.MapGet("/api/customers",
+        async (ICustomerService customerService) => await customerService.GetCustomersAsync());
+
+    app.MapGet("/api/customers/{username}",
+        async (string username, ICustomerService customerService) =>
+        await customerService.GetCustomerByUsernameAsync(username));
+    //app.MapPost("/", () => "Welcome to Customer API!");
+    //app.MapPut("/", () => "Welcome to Customer API!");
+    //app.MapDelete("/", () => "Welcome to Customer API!");
+
+
 
 
     app.UseHttpsRedirection();
